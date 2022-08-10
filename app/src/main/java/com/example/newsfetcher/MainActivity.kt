9af7060e57d007dcb39242f6a4dd620ec7760dfb
Your Name <applezafar@gmail.com>
2022-08-11 00:29:44 +1000

    Coroutines — часть 2

--- app/src/main/java/com/example/newsfetcher/MainActivity.kt
+++ app/src/main/java/com/example/newsfetcher/MainActivity.kt
@@ -2,12 +2,33 @@ package com.example.newsfetcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.newsfetcher.feature.mainscreen.MainScreenFragment
+import com.example.newsfetcher.feature.mainscreen.bookmarks.ui.BookmarksFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val bottomNavigationView: BottomNavigationView by lazy { findViewById(R.id.bnvBar) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.container, MainScreenFragment()).commit()
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.itemMain -> {
+                    selectTab(MainScreenFragment())
+                }
+                R.id.itemBookmarks -> {
+                    selectTab(BookmarksFragment())
+                }
+                else -> {}
+            }
+            true
+        }
+    bottomNavigationView.selectedItemId =R.id.itemMain
+    }
+    private fun selectTab(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+            .commit()
     }
 }
